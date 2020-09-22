d9ecd00de9de78fe9ef9d8a4eedd9cf758bc8d23
JoakimDenlov <57172462+JoakimDenlov@users.noreply.github.com>
2020-09-22 10:17:44 +0200

    Footer Added

--- src/view/ShopView/ShopView.js
+++ src/view/ShopView/ShopView.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
 import "./ShowView.css";
 import Perfumes from "../../components/Perfumes/Perfumes";
+import Footer from "../../components/Footer/Footer"
 
 const ShopView = (props) => {
   return (
     <div className="ShowView">
       <Perfumes perfumes={props.data} addToCart={props.addToCartHandler} />
+      <Footer />
     </div>
   );
 };
