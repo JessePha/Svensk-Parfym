2fbc67a77d338c0a00658cb591d0cd5a7899beaa
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-09-24 14:03:01 +0200

    Added aboutUs information-file

--- src/view/AboutUs View/AboutUs.js
+++ src/view/AboutUs View/AboutUs.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Information from "../../components/AboutUs Component/Information/Information";
 
 let AboutUs = () => {
   return (
     <div>
+    <Information/>
     </div>
   );
 };
