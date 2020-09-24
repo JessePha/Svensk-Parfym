e7bcd79aa39d205efedd7ab98f6a8bf081e7bb71
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-09-24 12:00:13 +0200

    More css improvments to the cart

--- src/components/CartProduct/CartProductRender/CartProductRender.js
+++ src/components/CartProduct/CartProductRender/CartProductRender.js
@@ -9,17 +9,13 @@ let AddRemoveItem = (props) => {
 
   let addFromItem = () => {
     console.log(props.products[props.index].price);
-    props.setTotalPrice(
-      (props.totalPrice + props.products[props.index].price)
-    );
+    props.setTotalPrice(props.totalPrice + props.products[props.index].price);
     setCurrentAmount(currentAmount + 1);
   };
 
   let subFromItem = () => {
     if (currentAmount !== 1) {
-      props.setTotalPrice(
-        props.totalPrice - props.products[props.index].price
-      );
+      props.setTotalPrice(props.totalPrice - props.products[props.index].price);
       setCurrentAmount(currentAmount - 1);
     }
   };
@@ -34,27 +30,39 @@ let AddRemoveItem = (props) => {
   };
   return (
     <div>
-      <p>{currentAmount}</p>
-      <img
-        className={classes.productImage}
-        src={props.products[props.index].img}
-        alt="product"
-      />
-      <p>{props.products[props.index].name}</p>
-      <p>{props.products[props.index].price} Kr</p>
-      <button onClick={addFromItem} className={classes.AddSubRemoveButtons}>
-        <FontAwesomeIcon icon={faPlus} />
-      </button>
-      <button onClick={subFromItem} className={classes.AddSubRemoveButtons}>
-        <FontAwesomeIcon icon={faMinus} />
-      </button>
-      <button
-        index={props.index}
-        onClick={removeItem}
-        className={classes.AddSubRemoveButtons}
-      >
-        Remove
-      </button>
+      <div className={classes.infoDiv}>
+        <img
+          className={classes.productImage}
+          src={props.products[props.index].img}
+          alt="product"
+        />
+        <div className={classes.dataDiv}>
+          <p>{props.products[props.index].name}</p>
+          <p>{props.products[props.index].price} Kr</p>
+          <div className={classes.buttonsDiv}>
+            <button
+              onClick={addFromItem}
+              className={classes.AddSubRemoveButtons}
+            >
+              <FontAwesomeIcon icon={faPlus} />
+            </button>
+            <p>{currentAmount}</p>
+            <button
+              onClick={subFromItem}
+              className={classes.AddSubRemoveButtons}
+            >
+              <FontAwesomeIcon icon={faMinus} />
+            </button>
+            <button
+              index={props.index}
+              onClick={removeItem}
+              className={classes.AddSubRemoveButtons}
+            >
+              Remove
+            </button>
+          </div>
+        </div>
+      </div>
       <hr />
     </div>
   );
