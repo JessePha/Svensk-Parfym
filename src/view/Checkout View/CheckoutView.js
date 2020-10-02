e31c13d1cc3c957e6487ab1294146689bd693a2a
JoakimDenlov <Joakimdenlov1@gmail.com>
2020-10-02 04:59:43 +0200

    Checkout form

---
+++ src/view/Checkout View/CheckoutView.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import CheckOut from "../../components/Checkout Component/Checkout";
+import Checkout from "../../components/Checkout Component/CheckoutForm";
+
+const CheckoutView = () => {
+  return (
+    <div>
+      <Checkout />
+    </div>
+  );
+};
+
+export default CheckoutView;
