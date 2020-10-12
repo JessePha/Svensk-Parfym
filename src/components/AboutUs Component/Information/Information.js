2c0b23ce57997fdb4f59006697d242fd291569c8
Anara23 <anarikrulit@mail.ru>
2020-10-12 20:54:15 +0200

    Anara

--- src/components/AboutUs Component/Information/Information.js
+++ src/components/AboutUs Component/Information/Information.js
@@ -1,45 +1,57 @@
 import classes from '../Information.module.css';
-import React, { Component } from 'react';
-import AboutUsPhoto from '../AboutUsPhoto/AboutUsPhoto';
-import photo1 from '../../../shared/Images/perfumer.jpg';
-import signature from '../../../shared/Images/signature.jpg';
+import React from 'react';
 
-import {text, text2, text3, text4, text5, text6, text7, text8, text9, text10, text11, text12} from '../../../shared/AboutUs.json';
+import photo1 from '../../../shared/Images/picture1.jpg';
+import photo2 from '../../../shared/Images/picture2.jpg';
+import photo4 from '../../../shared/Images/picture4.jpg';
 
-class Information extends Component {
-   
-  render() {
-    
+import {text, text2, text3, text4, text5} from '../../../shared/AboutUs.json';
+
+const Information = () => {
+
     return (
       <div className={classes.Information}>
+
+        <img src={photo1} className={classes.HeaderPhoto} alt="headerphoto" />
+
+        <div className={classes.Title}>About us</div>
+
+        <div className={classes.MainInformation}>
+          <div className={classes.TopText}>CONCEPT OF SVENSK PARFYM
+          <p>{text}</p>
+          </div>  
+        </div>
+
+        <diV className={classes.MainInformation}>
+          <div className={classes.TopText}>INVENTION
+          <p>{text2}</p>
+          </div>
+        </diV>
+
+        <img src={photo2} className={classes.Photo2} alt="Photo2" />
+
+        <diV className={classes.MainInformation}>
+          <div className={classes.TopText}>NICHE PERFUMERY
+          <p>{text3}</p>
+          </div>
+        </diV>
           
-          <div className={classes.Title}>ABOUT</div>
-
-          <div className={classes.TopText}>Perfumer Henrik Lestréus and the fine fragrances brand Svensk Parfym</div>
-
-          <AboutUsPhoto image={photo1} />
-        
-          <div className={classes.MainInformation}>
-            <p>{text}</p><br/>
-            <p>{text2}</p><br/>
-            <p>{text3}</p><br/>
-            <p>{text4}</p><br/>
-            <p>{text5}</p><br/>
-            <p>{text6}</p><br/>
-            <p>{text7}</p><br/>
-            <p>{text8}</p><br/>
-            <p>{text9}</p><br/>
-            <p>{text10}</p><br/>
-            <p>{text11}</p><br/>
-            <p>{text12}</p>
+        <diV className={classes.MainInformation}>
+          <div className={classes.TopText}>FRAGRANCE IS GENDER-FREE
+          <p>{text4}</p>
           </div>
+        </diV>
 
-          <div>
-            <img src={signature} alt="signature" className={classes.Signature}/>
-          </div>
+        <diV className={classes.MainInformation}>
+          <div className={classes.TopText}>INVENTION
+          <p>{text5}</p>
+        </div>
+
+        <img src={photo4} className={classes.Photo4} alt="Photo4" />
+          
+        </diV>
       </div>
-    );
-  };
-}
+    )
+  }
 
 export default Information;
