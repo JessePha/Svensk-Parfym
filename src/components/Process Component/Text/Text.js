28eee4902de43cd6a56b92ff7ecf1ad2b85f78cd
96simben <simoncraft3@hotmail.com>
2020-10-13 16:04:28 +0200

    lite css

--- src/components/Process Component/Text/Text.js
+++ src/components/Process Component/Text/Text.js
@@ -10,8 +10,8 @@ const Text = (props) => {
         <p className="breadText">
           With focus on usability and versatility Svensk Parfym is a fine
           fragrances brand - made in Sweden, inspired by Sweden. The line of
-          fragrances of Svensk Parfym will continue to grow as inspiration and
-          creativity comes along - always unique, generous and progressing.
+          fragrances of Svensk Parfym will continue to grow as <br/> inspiration and
+          creativity comes along - <br/> always unique, generous and progressing.
         </p>
         <img src={perfym1} alt="Flowers" className="pic" />
       </div>
@@ -23,7 +23,7 @@ const Text = (props) => {
           soul, either sitting on surfaces or floating around in the air. The
           volatility and behaviour of these fantastic molecules depend totally
           on their structures and on environmental physical circumstances,
-          temperature and humidity being the most important ones. All unique
+          temperature and humidity being the most important ones. <br/> All unique
           inventions needs bold creativity. Perfumery needs both unique
           inventions and total constancy - they are just different parts of the
           process; first invention then full systematic consistency.
@@ -36,7 +36,7 @@ const Text = (props) => {
         <p className="breadText">
           Niche perfumery is specialist perfumery with all focus on perfume.
           Designer perfumes comes from brands that sells all sorts of things;
-          clothes, shoes, bags, makeup, you name it. Svensk Parfym is definitely
+          clothes, shoes, bags, makeup, you name it. <br/> Svensk Parfym is definitely
           a niche brand in the sense that we focus entirely on fine fragrances.
           Niche is the best you can find if there is much dedication and talent
           behind it and consequently niche is very poor if there isn’t.
@@ -49,7 +49,7 @@ const Text = (props) => {
         <p className="breadText">
           We love differences. Svensk Parfym fragrances are categorised as
           gender-free to signalise that anyone anywhere should think and feel by
-          themselves what they want - freely and with all differences preserved.
+          themselves what they want - <br/> freely and with all differences preserved.
         </p>
         <img src={perfym1} alt="Flowers" className="pic" />
       </div>
