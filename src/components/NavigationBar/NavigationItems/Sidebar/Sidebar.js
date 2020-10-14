dac1e000bbbf34963423ec529a16fa8196242a95
Davidfurby <57434673+Davidfurby@users.noreply.github.com>
2020-10-14 17:09:44 +0200

    Added CloseSidebar-button

--- src/components/NavigationBar/NavigationItems/Sidebar/Sidebar.js
+++ src/components/NavigationBar/NavigationItems/Sidebar/Sidebar.js
@@ -3,6 +3,8 @@ import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems";
 import Backdrop from "../../../UI/Backdrop/Backdrop";
 import classes from "./Sidebar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faWindowClose } from "@fortawesome/free-solid-svg-icons";
 
 const SideBar = (props) => {
   let attachedClasses = [classes.SideDrawer, classes.Close];
@@ -10,9 +12,16 @@ const SideBar = (props) => {
     attachedClasses = [classes.SideDrawer, classes.Open];
   }
   return (
-    <div>
+    <div className={classes.sidebarDiv}>
       <Backdrop open={props.open} closed={props.closed} />
+
       <div className={attachedClasses.join(" ")}>
+        <span onClick={props.closed}>
+          <FontAwesomeIcon
+            className={classes.CloseButton}
+            icon={faWindowClose}
+          ></FontAwesomeIcon>
+        </span>
         <Logo />
         <NavigationItems />
       </div>
