a73bd9542878c5b45fdd2e0332efd6001e93be17
Jesse Pham <93jespha@mk.se>
2020-11-09 13:54:17 +0100

    Fix Active in inventPage

--- src/components/AboutUs Component/Information/TextBundles.js
+++ src/components/AboutUs Component/Information/TextBundles.js
@@ -3,6 +3,11 @@ import classes from "./Information.module.css";
 
 const Display = ({ textContent, defaultContent }) => {
   const [content, setContent] = useState(null);
+  const [defaultAcive, setDefaultActive] = useState(true);
+  const [active1, setActive1] = useState(false);
+  const [active2, setActive2] = useState(false);
+  const [active3, setActive3] = useState(false);
+  const [active4, setActive4] = useState(false);
   const onClick = (e) => {
     let text = [];
     if (e.target.value === "1") {
@@ -10,21 +15,43 @@ const Display = ({ textContent, defaultContent }) => {
         (content) => content[0] === "Scent" || content[0] === "Importance"
       );
       setContent(text);
+      setActive1(true);
+      setActive2(active2 ? false : active2);
+      setDefaultActive(false);
     } else if (e.target.value === "2") {
       text = textContent.filter(
         (content) => content[0] === "Expression" || content[0] === "Process"
       );
       setContent(text);
+      setActive2(true);
+      setActive1(active1 ? false : active1);
+      setDefaultActive(false);
     } else if (e.target.value === "3") {
       text = textContent.filter(
         (content) => content[0] === "Art" || content[0] === "Business"
       );
       setContent(text);
+      setActive3(true);
+      setActive1(active1 ? false : active1);
+      setActive2(active2 ? false : active2);
+      setDefaultActive(false);
     } else if (e.target.value === "4") {
       text = textContent.filter(
         (content) => content[0] === "Passion" || content[0] === "Brand"
       );
       setContent(text);
+      setActive4(true);
+      setActive1(active1 ? false : active1);
+      setActive2(active2 ? false : active2);
+      setActive3(active3 ? false : active3);
+    }
+    else{
+      setContent(null)
+      setDefaultActive(true)
+      setActive1(active1 ? false : active1);
+      setActive2(active2 ? false : active2);
+      setActive3(active3 ? false : active3);
+      setActive4(active4 ? false : active4);
     }
   };
 
@@ -32,22 +59,39 @@ const Display = ({ textContent, defaultContent }) => {
     <div>
       <div className={classes.ButtonDiv}>
         <button
-          className={classes.Buttons}
+          className={
+            defaultAcive
+              ? `${classes.Buttons} ${classes.Active}`
+              : classes.Buttons
+          }
+          value="0"
+          onClick={(e) => onClick(e)}
+        />
+        <button
+          className={
+            active1 ? `${classes.Buttons} ${classes.Active}` : classes.Buttons
+          }
           value="1"
           onClick={(e) => onClick(e)}
         />
         <button
-          className={classes.Buttons}
+          className={
+            active2 ? `${classes.Buttons} ${classes.Active}` : classes.Buttons
+          }
           value="2"
           onClick={(e) => onClick(e)}
         />
         <button
-          className={classes.Buttons}
+          className={
+            active3 ? `${classes.Buttons} ${classes.Active}` : classes.Buttons
+          }
           value="3"
           onClick={(e) => onClick(e)}
         />
         <button
-          className={classes.Buttons}
+          className={
+            active4 ? `${classes.Buttons} ${classes.Active}` : classes.Buttons
+          }
           value="4"
           onClick={(e) => onClick(e)}
         />
@@ -56,7 +100,7 @@ const Display = ({ textContent, defaultContent }) => {
         <div className={classes.defaultText}>{defaultContent}</div>
       ) : (
         <div className={classes.TextBundleDiv}>
-          { content.map((text) => (
+          {content.map((text) => (
             <div className={classes.textContent} key={text[0]}>
               {" "}
               <h2>{text[0]}</h2> <p>{text[1]}</p>{" "}
