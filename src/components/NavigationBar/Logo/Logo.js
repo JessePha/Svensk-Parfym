62a1fbfbdf252e6da5f854d819180084d9c8d507
Jesse Pham <93jespha@mk.se>
2020-11-09 15:34:26 +0100

    get imgs from Fstore. Remove shared folder.

--- src/components/NavigationBar/Logo/Logo.js
+++ src/components/NavigationBar/Logo/Logo.js
@@ -1,14 +1,13 @@
 import React from "react";
-import Logo from "../../../shared/Images/newLogo.jpg";
 import { Link } from "react-router-dom";
 import "./Logo.module.css";
 import classes from "./Logo.module.css";
 
-const logo = ({toggleSidebar}) => {
+const logo = ({toggleSidebar, logo}) => {
   return (
     <div className={classes.Logo}>
       <Link to="/">
-        <img src={Logo} alt="Logo" onClick = {toggleSidebar} />
+        <img src={logo} alt="Logo" onClick = {toggleSidebar} />
       </Link>
     </div>
   );
--- src/components/NavigationBar/Toolbar/Toolbar.js
+++ src/components/NavigationBar/Toolbar/Toolbar.js
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from "react";
 import classes from "./Toolbar.module.css";
 import Burger from "../NavigationItems/Sidebar/SidebarIcon/SidebarIcon";
 import NavigationItems from "../NavigationItems/NavigationItems";
-import { HiOutlineShoppingBag } from 'react-icons/hi';
-import logoimg from "../../../shared/Images/newLogo.jpg";
-import logoimg1 from "../../../shared/Images/Ny_SP.png";
+import { HiOutlineShoppingBag } from "react-icons/hi";
+import { projectFirestore } from "../../../firestore/config";
 import Logo from "../Logo/Logo";
 
 const Toolbar = (props) => {
+  const [navPic, setNavPic] = useState([]);
   const [show, setShow] = useState({
     visible: true,
     prevScroll: window.pageYOffset,
   });
 
-  const closeSideAndOpenCartBar = () =>{
-    props.cartToggle()
-    props.closeSidebar()
-  }
+  const closeSideAndOpenCartBar = () => {
+    props.cartToggle();
+    props.closeSidebar();
+  };
 
   useEffect(() => {
     const handScroll = () => {
@@ -31,26 +31,49 @@ const Toolbar = (props) => {
     };
   }, [show]);
 
-  return (
-    <header className={show.visible ? classes.Toolbar : classes.Toolbarhidden}>
-      <div className = {classes.LogoAndNav}>
-        <Logo logo={logoimg} />
-        <div className={classes.NavigationItems}>
-          <NavigationItems/>
+  useEffect(() => {
+    const fetchItem = () => {
+      projectFirestore
+        .collection("navigation")
+        .get()
+        .then((querySnapShot) => {
+          querySnapShot.forEach((doc) => {
+            setNavPic(doc.data());
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchItem();
+  }, []);
+  let header = null;
+  if (navPic.url) {
+    header = (
+      <header
+        className={show.visible ? classes.Toolbar : classes.Toolbarhidden}
+      >
+        <div className={classes.LogoAndNav}>
+          <Logo logo={navPic.url[0]} />
+          <div className={classes.NavigationItems}>
+            <NavigationItems />
+          </div>
         </div>
-      </div>
-      <img src={logoimg1} alt="logo" className={classes.LogoName} />
-      <div className={classes.Cartdiv}>
-        {props.amountInCart > 0 ? <span>{props.amountInCart}</span> : null}
-        <HiOutlineShoppingBag
-          className={classes.Cart}
-          onClick={() => closeSideAndOpenCartBar()}
-          amount={props.amountInCart}
-        />
-        <Burger clicked={props.toggleSidebar} isOpened = {props.sideBar}/>
-      </div>
-    </header>
-  );
+        <img src={navPic.url[1]} alt="logo" className={classes.LogoName} />
+        <div className={classes.Cartdiv}>
+          {props.amountInCart > 0 ? <span>{props.amountInCart}</span> : null}
+          <HiOutlineShoppingBag
+            className={classes.Cart}
+            onClick={() => closeSideAndOpenCartBar()}
+            amount={props.amountInCart}
+          />
+          <Burger clicked={props.toggleSidebar} isOpened={props.sideBar} />
+        </div>
+      </header>
+    );
+  }
+
+  return <> {header} </>;
 };
 
 export default Toolbar;
